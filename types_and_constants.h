96c70783929e988f38884e7e352f022060fcc737
Marmotikon <msemenov93057@gmail.com>
2022-03-17 02:22:39 +0300

    Removed the confusion between component ID and component itself

--- types_and_constants.h
+++ types_and_constants.h
@@ -3,10 +3,11 @@
 #include <bitset>
 #include <cstdint>
 
-const uint16_t kMaxEntities = 1000;
-const uint8_t kMaxComponents = 10;
-
-using id_type = uint16_t;  // type for all indexes in ComponentArray, Entity etc
 using Entity = uint16_t;
-using Component = uint8_t;
+using ComponentID = uint8_t;
+using id_type = uint16_t;  // type for all indexes in ComponentArray, Entity etc
+
+const Entity kMaxEntities = 1000;
+const ComponentID kMaxComponents = 10;
+
 using ComponentSignature = std::bitset<kMaxComponents>;
