35c8ba57c6cfd57b1997b622b7138f8a6b52ae68
tiliguzov <tiliguzovegor@yandex.ru>
2022-05-14 23:06:33 +0300

    Add grid, and ability to add textures

--- map_creator.h
+++ map_creator.h
@@ -4,6 +4,7 @@
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QListWidget>
+#include <QMouseEvent>
 
 class MapCreator : public QWidget {
   Q_OBJECT
@@ -11,9 +12,17 @@ class MapCreator : public QWidget {
  public:
   MapCreator(QWidget* parent);
   void LoadTextures();
+  void CreateGrid();
+  void AddTexture();
+  QPointF MousePositionOnSceneView();
+
+  void mousePressEvent(QMouseEvent* event) override;
 
  private:
   QGraphicsScene* scene_;
   QGraphicsView* scene_view_;
   QListWidget* list_of_textures_;
+  bool new_texture_on{false};
+  QPixmap* new_texture_;
+  QPixmap* ground_;
 };
