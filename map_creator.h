711014d90df19520c4ac4c03ab21cc12c4bc2815
tiliguzov <tiliguzovegor@yandex.ru>
2022-05-15 01:25:16 +0300

    Add ability to select layer, and to delete texture.

--- map_creator.h
+++ map_creator.h
@@ -5,24 +5,31 @@
 #include <QGraphicsScene>
 #include <QListWidget>
 #include <QMouseEvent>
+#include <QJsonObject>
+#include <QPushButton>
+#include <vector>
 
 class MapCreator : public QWidget {
   Q_OBJECT
 
  public:
-  MapCreator(QWidget* parent);
+  explicit MapCreator(QWidget* parent);
   void LoadTextures();
   void CreateGrid();
   void AddTexture();
+  void DeleteTexture();
   QPointF MousePositionOnSceneView();
+  static QJsonObject GetJsonObject(const std::string& path_to_json);
 
   void mousePressEvent(QMouseEvent* event) override;
 
  private:
   QGraphicsScene* scene_;
   QGraphicsView* scene_view_;
-  QListWidget* list_of_textures_;
+  QListWidget* list_of_textures_[3]{};
   bool new_texture_on{false};
-  QPixmap* new_texture_;
-  QPixmap* ground_;
+  QPixmap new_texture_;
+  QPushButton* layer_button_;
+  int layer_{0};
+  std::vector <QGraphicsPixmapItem*> items_;
 };
